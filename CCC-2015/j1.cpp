dd9e65ca05da86efa2c188f0f3f765772d5ac8a9
SpaceFox2050 <87918634+SpaceFox2050@users.noreply.github.com>
2021-09-22 08:40:34 -0700

    Update j1.cpp

--- CCC-2015/j1.cpp
+++ CCC-2015/j1.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+    int month, day;
+    cin >> month >> day;
+    if(month == 2 && day == 18){
+        cout << "Special";
+    }else if((month == 2 && day > 18) || month > 2){
+        cout << "Before";
+    }else{
+        cout << "After";
+    }
+    return 0;
+}  
