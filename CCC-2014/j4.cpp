b07e8a2c1128386b9fb131136339722e5ca9a81b
SpaceFox2050 <87918634+SpaceFox2050@users.noreply.github.com>
2021-09-22 08:36:54 -0700

    Create j4.cpp

---
+++ CCC-2014/j4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int friends, removals, temp;
+    cin >> friends >> removals;
+    vector<int> nums;
+    for(int x = 1; x < friends+1; x++)
+        nums.push_back(x);
+    for(int x = 0; x < removals; x++){
+        cin >> temp;
+        for(int y = temp-1; y < nums.size(); y+=temp-1){
+            nums.erase(nums.begin()+y);
+        }
+    }
+    for(int num : nums)
+        cout << num << endl;
+    return 0;
+}  
